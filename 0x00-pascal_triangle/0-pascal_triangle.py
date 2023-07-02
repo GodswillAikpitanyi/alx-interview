d6b685cf4123f7f1c7d4cc6585919f0fba56cc90
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-02 16:17:03 +0000

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Generate Pascal's triangle up to the given number of rows."""
 def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the given number of rows.
