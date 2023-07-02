7eb63a304c28e26da77db60bbca28874a5de1a8e
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-02 16:27:57 +0000

    pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Generate Pascal's triangle up to the given number of rows."""
+
+
 def pascal_triangle(n):
     """
     Generate Pascal's triangle up to the given number of rows.
@@ -15,7 +17,7 @@ def pascal_triangle(n):
         # Returns an empty list if n <= 0
         return []
     # Start with the first row [1]
-    triangle = [[1]]  
+    triangle = [[1]]
 
     for i in range(1, n):
         # Gets the previous row
@@ -24,11 +26,11 @@ def pascal_triangle(n):
         curr_row = [1]
 
         for j in range(1, i):
-            # Computes the values by adding adjacent elements from the previous row
+            # Computes the values by adding adjacent elements from the prev row
             curr_row.append(prev_row[j-1] + prev_row[j])
-         # End the current row with 1
+            # End the current row with 1
         curr_row.append(1)
         # Add the current row to the triangle
         triangle.append(curr_row)
-     # Return the resulting triangle
+    # Return the resulting triangle
     return triangle
