4be1e6be535b1a67703b3c044613e289c619b5d6
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-07-06 22:07:15 +0000

    PEP8 update

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,19 +9,21 @@ def canUnlockAll(boxes):
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-
     num_boxes = len(boxes)
+    if num_boxes == 0:
+        return False
+
     unlocked_boxes = [False] * num_boxes
     unlocked_boxes[0] = True
 
-    # Starts with the first box
-    stack = [0]
+    stack = [0]  # Start with the first box
     while stack:
         current_box = stack.pop()
         keys = boxes[current_box]
         for key in keys:
-            if key >= 0 and key < num_boxes and not unlocked_boxes[key]:
+            if 0 <= key < num_boxes and not unlocked_boxes[key]:
                 unlocked_boxes[key] = True
                 stack.append(key)
 
     return all(unlocked_boxes)
+
