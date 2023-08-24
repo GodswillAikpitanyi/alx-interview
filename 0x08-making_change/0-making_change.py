9ee5d8a823144364e167cd0b6eb22132814d516f
Godswill Aikpitanyi <godswillaikpitanyi@gmail.com>
2023-08-24 21:45:50 +0000

    Change comes from within

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Change comes from within
+"""
+
+def makeChange(coins, total):
+    """
+    Returns the fewest number of coins needed to make a change
+    """
+    if total <= 0:
+        return 0
+
+    temporary_value = 0
+    coins.sort(reverse=True)
+
+    for coin in coins:
+        if total >= coin:
+            temporary_value += total // coin
+            total = total % coin
+
+    return temporary_value if total == 0 else -1
+
